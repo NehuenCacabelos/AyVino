f1f2295bed0d191f7cd6dcfad61294df7f8e6897
Nehuen <riveronehuen400@gmail.com>
2026-08-31 09:18:29 -0300

    Union

--- src/backend/AyVino.Api/Common/Constants/AppRoles.cs
+++ src/backend/AyVino.Api/Common/Constants/AppRoles.cs
@@ -5,4 +5,8 @@ public static class AppRoles
     public const string User = "User";
     public const string Winery = "Winery";
     public const string Admin = "Admin";
+    public const string Admin = "NOmbre";
+    public const string Admin = "Nombre";
+
+
 }
